23b3b8a00289a869a511257d95b979ee23ecae22
Palmer Harrell <palmerharrell@me.com>
2016-02-17 14:59:47 -0600

    Parsed JSON file, loaded into songs array and populated List View in the DOM

--- scripts/processJSON.js
+++ scripts/processJSON.js
@@ -4,16 +4,16 @@ songList1.addEventListener("load", parseSongList);
 songList1.open("GET", "json/songs1.json");
 songList1.send();
 
-// Parse JSON and populate songs array
+// Parse JSON, populate songs array, refresh Song List view in DOM
 function parseSongList() {
 	var data = JSON.parse(this.responseText);
-	console.log("SongList1:", data);
-	// Create string like: Legs - by ZZ Top on the album Eliminator
 	for (var i = 0; i < data.songs.length; i++) {
 		var currentSong = data.songs[i];
-		console.log(`Title ${i}: `, currentSong.title);
-		console.log(`Artist ${i}: `, currentSong.artist);
-		console.log(`Album ${i}: `, currentSong.album);
-		console.log(`------------------`);
+		var songString = ``;
+		songString += `${currentSong.title} - by ${currentSong.artist} `;
+		songString += `on the album ${currentSong.album}`;
+		songs.push(songString);
 	};
-}
+	refreshSongList();
+};
+
